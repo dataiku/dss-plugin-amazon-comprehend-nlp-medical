4c57bf21e10743b996edbe542d437816747cc7de
alexcombessie <alex.combessie@gmail.com>
2020-05-07 20:06:48 +0200

    working PHI recipe

--- custom-recipes/amazon-comprehend-medical-protected-health-information/recipe.py
+++ custom-recipes/amazon-comprehend-medical-protected-health-information/recipe.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
-from typing import List, Dict, AnyStr, Union
+from typing import Dict, AnyStr
 
 from retry import retry
 from ratelimit import limits, RateLimitException
@@ -18,11 +18,7 @@ from dataiku.customrecipe import (
     get_input_names_for_role,
     get_output_names_for_role,
 )
-from api_formatting import (
-    EntityTypeEnum,
-    get_client,
-    NamedEntityRecognitionAPIFormatter,
-)
+from api_formatting import get_client, MedicalPhiAPIFormatter
 
 
 # ==============================================================================
@@ -35,29 +31,20 @@ api_quota_period = api_configuration_preset.get("api_quota_period")
 parallel_workers = api_configuration_preset.get("parallel_workers")
 batch_size = api_configuration_preset.get("batch_size")
 text_column = get_recipe_config().get("text_column")
-text_language = get_recipe_config().get("language")
-language_column = get_recipe_config().get("language_column")
-entity_types = [EntityTypeEnum[i] for i in get_recipe_config().get("entity_types", [])]
 error_handling = ErrorHandlingEnum[get_recipe_config().get("error_handling")]
 
 input_dataset_name = get_input_names_for_role("input_dataset")[0]
 input_dataset = dataiku.Dataset(input_dataset_name)
 input_schema = input_dataset.read_schema()
 input_columns_names = [col["name"] for col in input_schema]
+validate_column_input(text_column, input_columns_names)
 
 output_dataset_name = get_output_names_for_role("output_dataset")[0]
 output_dataset = dataiku.Dataset(output_dataset_name)
 
-validate_column_input(text_column, input_columns_names)
-
-api_support_batch = True
-if text_language == "language_column":
-    api_support_batch = False
-    validate_column_input(language_column, input_columns_names)
-
 input_df = input_dataset.get_dataframe()
-client = get_client(api_configuration_preset, "comprehend")
-column_prefix = "entity_api"
+client = get_client(api_configuration_preset, "comprehendmedical")
+column_prefix = "medical_phi_api"
 
 
 # ==============================================================================
@@ -67,53 +54,26 @@ column_prefix = "entity_api"
 
 @retry((RateLimitException, OSError), delay=api_quota_period, tries=5)
 @limits(calls=api_quota_rate_limit, period=api_quota_period)
-def call_api_named_entity_recognition(
-    text_column: AnyStr,
-    text_language: AnyStr,
-    language_column: AnyStr = None,
-    row: Dict = None,
-    batch: List[Dict] = None,
-) -> List[Union[Dict, AnyStr]]:
-    if text_language == "language_column":
-        # Cannot use batch as language may be different for each row
-        text = row[text_column]
-        language_code = row[language_column]
-        empty_conditions = [
-            not (isinstance(text, str)),
-            not (isinstance(language_code, str)),
-            str(text).strip() == "",
-            str(language_code).strip() == "",
-        ]
-        if any(empty_conditions):
-            return ""
-        response = client.detect_entities(Text=text, LanguageCode=language_code)
-        return json.dumps(response)
-    else:
-        text_list = [str(r.get(text_column, "")).strip() for r in batch]
-        responses = client.batch_detect_entities(
-            TextList=text_list, LanguageCode=text_language
-        )
-        return responses
+def call_api_medical_phi_extraction(row: Dict, text_column: AnyStr) -> Dict:
+    text = row[text_column]
+    if not isinstance(text, str) or str(text).strip() == "":
+        return ""
+    responses = client.detect_phi(Text=text)
+    return json.dumps(responses)
 
 
 df = api_parallelizer(
     input_df=input_df,
-    api_call_function=call_api_named_entity_recognition,
+    api_call_function=call_api_medical_phi_extraction,
     text_column=text_column,
-    text_language=text_language,
-    language_column=language_column,
     parallel_workers=parallel_workers,
-    api_support_batch=api_support_batch,
     batch_size=batch_size,
     error_handling=error_handling,
     column_prefix=column_prefix,
 )
 
-api_formatter = NamedEntityRecognitionAPIFormatter(
-    input_df=input_df,
-    entity_types=entity_types,
-    column_prefix=column_prefix,
-    error_handling=error_handling,
+api_formatter = MedicalPhiAPIFormatter(
+    input_df=input_df, column_prefix=column_prefix, error_handling=error_handling,
 )
 output_df = api_formatter.format_df(df)
 
--- python-lib/api_formatting.py
+++ python-lib/api_formatting.py
@@ -24,26 +24,32 @@ from plugin_io_utils import (
 
 API_EXCEPTIONS = (Boto3Error, BotoCoreError, ClientError)
 
-API_SUPPORT_BATCH = True
-BATCH_RESULT_KEY = "ResultList"
-BATCH_ERROR_KEY = "ErrorList"
-BATCH_INDEX_KEY = "Index"
-BATCH_ERROR_MESSAGE_KEY = "ErrorMessage"
-BATCH_ERROR_TYPE_KEY = "ErrorCode"
+API_SUPPORT_BATCH = False
+BATCH_RESULT_KEY = None
+BATCH_ERROR_KEY = None
+BATCH_INDEX_KEY = None
+BATCH_ERROR_MESSAGE_KEY = None
+BATCH_ERROR_TYPE_KEY = None
 
 VERBOSE = False
 
 
-class EntityTypeEnum(Enum):
-    COMMERCIAL_ITEM = "Commercial item"
+class MedicalEntityTypeEnum(Enum):
+    ANATOMY = "Anatomy"
+    MEDICAL_CONDITION = "Medical condition"
+    MEDICATION = "Medication"
+    PROTECTED_HEALTH_INFORMATION = "Protected health information"
+    TEST_TREATMENT_PROCEDURE = "Test treatment procedure"
+    TIME_EXPRESSION = "Time expression"
+
+
+class MedicalPHITypeEnum(Enum):
+    AGE = "Age"
     DATE = "Date"
-    EVENT = "Event"
-    LOCATION = "Location"
-    ORGANIZATION = "Organization"
-    OTHER = "Other"
-    PERSON = "Person"
-    QUANTITY = "Quantity"
-    TITLE = "Title"
+    NAME = "Mame"
+    PHONE_OR_FAX = "Phone or fax"
+    EMAIL = "Email"
+    ID = "ID"
 
 
 # ==============================================================================
@@ -62,144 +68,21 @@ def get_client(api_configuration_preset, service_name: AnyStr):
     return client
 
 
-class LanguageDetectionAPIFormatter:
+class MedicalPhiAPIFormatter:
     """
-    Formatter class for Language Detection API responses:
+    Formatter class for Protected Health Information API responses:
     - make sure response is valid JSON
-    - extract language code from response
+    - expand results to multiple columns
     - compute column descriptions
     """
 
     def __init__(
         self,
         input_df: pd.DataFrame,
-        column_prefix: AnyStr = "lang_detect_api",
+        column_prefix: AnyStr = "medical_phi_api",
         error_handling: ErrorHandlingEnum = ErrorHandlingEnum.LOG,
     ):
         self.input_df = input_df
-        self.column_prefix = column_prefix
-        self.error_handling = error_handling
-        self.api_column_names = build_unique_column_names(input_df, column_prefix)
-        self.language_code_column = generate_unique(
-            "language_code", input_df.keys(), self.column_prefix
-        )
-        self.language_score_column = generate_unique(
-            "language_score", input_df.keys(), self.column_prefix
-        )
-        self.column_description_dict = self._compute_column_description()
-
-    def _compute_column_description(self):
-        column_description_dict = {
-            v: API_COLUMN_NAMES_DESCRIPTION_DICT[k]
-            for k, v in self.api_column_names._asdict().items()
-        }
-        column_description_dict[
-            self.language_code_column
-        ] = "Language code from the API in ISO 639 format"
-        column_description_dict[
-            self.language_score_column
-        ] = "Confidence score of the API from 0 to 1"
-        return column_description_dict
-
-    def format_row(self, row: Dict) -> Dict:
-        raw_response = row[self.api_column_names.response]
-        response = safe_json_loads(raw_response, self.error_handling)
-        row[self.language_code_column] = ""
-        row[self.language_score_column] = None
-        languages = response.get("Languages", [])
-        if len(languages) != 0:
-            row[self.language_code_column] = languages[0].get("LanguageCode", "")
-            row[self.language_score_column] = languages[0].get("Score", None)
-        return row
-
-    def format_df(self, df: pd.DataFrame) -> pd.DataFrame:
-        logging.info("Formatting API results...")
-        df = df.apply(func=self.format_row, axis=1)
-        df = move_api_columns_to_end(df, self.api_column_names)
-        logging.info("Formatting API results: Done.")
-        return df
-
-
-class SentimentAnalysisAPIFormatter:
-    """
-    Formatter class for Sentiment Analysis API responses:
-    - make sure response is valid JSON
-    - extract sentiment scores from response
-    - compute column descriptions
-    """
-
-    def __init__(
-        self,
-        input_df: pd.DataFrame,
-        column_prefix: AnyStr = "sentiment_api",
-        error_handling: ErrorHandlingEnum = ErrorHandlingEnum.LOG,
-    ):
-        self.input_df = input_df
-        self.column_prefix = column_prefix
-        self.error_handling = error_handling
-        self.api_column_names = build_unique_column_names(input_df, column_prefix)
-        self.sentiment_prediction_column = generate_unique(
-            "prediction", input_df.keys(), column_prefix
-        )
-        self.sentiment_score_column_dict = {
-            p: generate_unique("score_" + p.lower(), input_df.keys(), column_prefix)
-            for p in ["Positive", "Neutral", "Negative", "Mixed"]
-        }
-        self.column_description_dict = self._compute_column_description()
-
-    def _compute_column_description(self):
-        column_description_dict = {
-            v: API_COLUMN_NAMES_DESCRIPTION_DICT[k]
-            for k, v in self.api_column_names._asdict().items()
-        }
-        column_description_dict[
-            self.sentiment_prediction_column
-        ] = "Sentiment prediction from the API (POSITIVE/NEUTRAL/NEGATIVE/MIXED)"
-        for prediction, column_name in self.sentiment_score_column_dict.items():
-            column_description_dict[
-                column_name
-            ] = "Confidence score in the {} prediction from 0 to 1".format(
-                prediction.upper()
-            )
-        return column_description_dict
-
-    def format_row(self, row: Dict) -> Dict:
-        raw_response = row[self.api_column_names.response]
-        response = safe_json_loads(raw_response, self.error_handling)
-        row[self.sentiment_prediction_column] = response.get("Sentiment", "")
-        sentiment_score = response.get("SentimentScore", {})
-        for prediction, column_name in self.sentiment_score_column_dict.items():
-            row[column_name] = None
-            score = sentiment_score.get(prediction)
-            if score is not None:
-                row[column_name] = round(score, 3)
-        return row
-
-    def format_df(self, df: pd.DataFrame) -> pd.DataFrame:
-        logging.info("Formatting API results...")
-        df = df.apply(func=self.format_row, axis=1)
-        df = move_api_columns_to_end(df, self.api_column_names)
-        logging.info("Formatting API results: Done.")
-        return df
-
-
-class NamedEntityRecognitionAPIFormatter:
-    """
-    Formatter class for Named Entity Recognition API responses:
-    - make sure response is valid JSON
-    - expand results to multiple columns (one by entity type)
-    - compute column descriptions
-    """
-
-    def __init__(
-        self,
-        input_df: pd.DataFrame,
-        entity_types: List,
-        column_prefix: AnyStr = "entity_api",
-        error_handling: ErrorHandlingEnum = ErrorHandlingEnum.LOG,
-    ):
-        self.input_df = input_df
-        self.entity_types = entity_types
         self.column_prefix = column_prefix
         self.error_handling = error_handling
         self.api_column_names = build_unique_column_names(input_df, column_prefix)
@@ -210,26 +93,33 @@ class NamedEntityRecognitionAPIFormatter:
             v: API_COLUMN_NAMES_DESCRIPTION_DICT[k]
             for k, v in self.api_column_names._asdict().items()
         }
-        for n, m in EntityTypeEnum.__members__.items():
+        for entity_enum in MedicalPHITypeEnum:
             entity_type_column = generate_unique(
-                "entity_type_" + n.lower(), self.input_df.keys(), self.column_prefix
+                "entity_type_" + str(entity_enum.value).lower() + "_text",
+                self.input_df.keys(),
+                self.column_prefix,
             )
             column_description_dict[
                 entity_type_column
-            ] = "List of '{}' entities recognized by the API".format(str(m.value))
+            ] = "List of '{}' PHI entities extracted by the API".format(
+                str(entity_enum.value)
+            )
         return column_description_dict
 
     def format_row(self, row: Dict) -> Dict:
         raw_response = row[self.api_column_names.response]
         response = safe_json_loads(raw_response, self.error_handling)
         entities = response.get("Entities", [])
-        selected_entity_types = sorted([e.name for e in self.entity_types])
-        for n in selected_entity_types:
+        for entity_enum in MedicalPHITypeEnum:
             entity_type_column = generate_unique(
-                "entity_type_" + n.lower(), row.keys(), self.column_prefix
+                "entity_type_" + str(entity_enum.value).lower() + "_text",
+                row.keys(),
+                self.column_prefix,
             )
             row[entity_type_column] = [
-                e.get("Text") for e in entities if e.get("Type", "") == n
+                e.get("Text", "")
+                for e in entities
+                if e.get("Type", "") == entity_enum.name
             ]
             if len(row[entity_type_column]) == 0:
                 row[entity_type_column] = ""
